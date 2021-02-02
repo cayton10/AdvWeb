ead40a8bcdf10dc68402e4e69eb87ade118c218d
Benjamin Paul Cayton <cayton10@live.marshall.edu>
2021-02-02 15:50:01 -0500

    Hope this fixed everything

--- Development/www/test_db_416.php
+++ Development/www/test_db_416.php
@@ -1,12 +1,16 @@
 <? 
-     	$mysqli = new mysqli("localhost", "my_user", "my_password", "userdb");
+     	$mysqli = new mysqli("dbServer", "my_user", "my_password", "userdb");
+
+		 
      
 	/* check connection */ 
      	if (mysqli_connect_errno()) 
      	{
 		echo "Connect failed: " . mysqli_connect_error() . "<br />"; 
 		exit(); 
-     	} 
+     	}
+
+		 
  
      	/* Create table and Insert rows */ 
      	if (!$mysqli->query("CREATE TABLE friends (id int, name varchar(20), 
@@ -18,12 +22,16 @@
           (7, 'Olivia')"))
 		$mysqli->error;
 
+		
      	/* select all rows */ 
-     	$result = $mysqli->query("SELECT * FROM friends");  
+     	$result = $mysqli->query("SELECT * FROM friends") or die($mysqli->error); 
+		 	 
      
 	echo "Affected rows by (SELECT): " . $mysqli->affected_rows . "<br />";
      	while($row = $result->fetch_assoc()) 
-          	echo stripslashes($row['name']) . "<br />";	
+          	echo stripslashes($row['name']) . "<br />";
+
+			  
      
 	/* close connection */ 
      	$mysqli->close(); 
